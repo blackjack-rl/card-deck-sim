eb4be6000125c3baf83c3287a400fbe228e80e22
Alex Zharichenko <azharichenko@gmail.com>
2021-10-16 14:00:17 -0400

    Created simple deck

--- deck.py
+++ deck.py
@@ -1,21 +1,23 @@
 from abc import ABC, abstractmethod
-from enum import Enum, IntEnum, auto
-from random import choice
-from typing import List, NamedTuple
+from collections import deque
+from enum import Enum, IntEnum
+from random import shuffle
+from secrets import choice
+from typing import Deque, List, NamedTuple
 
 
-class Suit(Enum):
-    SPADE = auto()
-    DIAMOND = auto()
-    HEART = auto()
-    CLUB = auto()
+class Suit(IntEnum):
+    SPADE = 0
+    DIAMOND = 13
+    HEART = 26
+    CLUB = 39
 
     def __repr__(self) -> str:
         return self.name
 
 
 class Face(IntEnum):
-    ACE = -1
+    ACE = 1
     TWO = 2
     THREE = 3
     FOUR = 4
@@ -25,9 +27,9 @@ class Face(IntEnum):
     EIGHT = 8
     NINE = 9
     TEN = 10
-    JACK = 10
-    QUEEN = 10
-    KING = 10
+    JACK = 11
+    QUEEN = 12
+    KING = 13
 
     def __repr__(self) -> str:
         return self.name
@@ -36,6 +38,9 @@ class Face(IntEnum):
 class Card(NamedTuple):
     suit: Suit
     face: Face
+
+    def id(self):
+        return self.suit + self.face
 
 
 def generate_card_deck() -> List[Card]:
@@ -51,11 +56,17 @@ def generate_card_deck() -> List[Card]:
 
 class Deck(ABC):
     def __init__(self, num_of_decks=1) -> None:
-        self.num_of_decks = num_of_decks
+        self.__num_of_decks = num_of_decks
 
     @abstractmethod
-    def draw_card(self):
+    def draw_card(self) -> Card:
         pass
+
+    def _generate_deck(self) -> List[Card]:
+        cards = []
+        for _ in range(self.__num_of_decks):
+            cards += generate_card_deck()
+        return cards
 
 
 class ContinuousShuffleMachine(Deck):
@@ -66,11 +77,13 @@ class ContinuousShuffleMachine(Deck):
 class StandardDeck(Deck):
     def __init__(self, num_of_decks=1) -> None:
         super().__init__(num_of_decks=num_of_decks)
-        self.deck = [1, 2, 3, 4, 5]
-        # self.discard_pile = []
+        self.discard_pile: Deque[Card] = deque()
+        self.deck: Deque[Card] = deque(self._generate_deck())
+        shuffle(self.deck)
 
     def draw_card(self):
-        card = choice(self.deck)
+        card = self.deck.popleft()
+        self.discard_pile.append(card)
         return card
 
 
@@ -78,5 +91,6 @@ if __name__ == "__main__":
     from pprint import pprint
 
     deck = StandardDeck()
-    print(deck.draw_card())
-    pprint(generate_card_deck())
+    card = deck.draw_card()
+    print(card, card.id())
+    print(deck.discard_pile)
